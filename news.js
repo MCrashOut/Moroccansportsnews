c3a010573f3ad38ee77ed44c6ec0f88e15fdbd10
Rabii <134378664+MCrashOut@users.noreply.github.com>
2026-03-31 21:31:10 +0100

    Delete news.js

--- news.js
+++
@@ -1,8 +0,0 @@
-// ===== NEWS =====
-
-// We removed broken RSS proxies
-// News now opens Google News searches
-
-function openNews(query) {
-  window.open(`https://www.google.com/search?q=${encodeURIComponent(query)}&tbm=nws`, "_blank");
-}
